2dcd4d01e0fa8a3570e896ae684b95dcaa67c06f
Salvatore <pigrecoinfinito@gmail.com>
2017-06-26 23:06:59 +0200

    query per registrare la geometria della view
    Serve per inserire la colonna `geom` nella tabella `views_geometry_columns` altrimenti QGIS la vede come semplice tabella!!!

---
+++ registrare_view.sql
@@ -0,0 +1,3 @@
+INSERT INTO views_geometry_columns
+(view_name, view_geometry, view_rowid, f_table_name, f_geometry_column, read_only)
+VALUES ('v_punti_reticolo', 'geom', 'pk_p', 'punti', 'geom',1)
